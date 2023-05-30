688cfcb6c744998d16e4889a33a765b9469d24eb
BrunOshiro <brunoshiro93@gmail.com>
2023-05-30 10:44:19 -0300

    fix - não estava sendo gerado o id da entity durante o mapeamento domainToEntity

--- src/main/java/com/jazztech/creditanalysis/infrastructure/repository/CreditAnalysisMapper.java
+++ src/main/java/com/jazztech/creditanalysis/infrastructure/repository/CreditAnalysisMapper.java
@@ -6,11 +6,19 @@ import com.jazztech.creditanalysis.presentation.dto.RequestDto;
 import com.jazztech.creditanalysis.presentation.dto.ResponseDto;
 import jakarta.validation.Valid;
 import java.util.List;
+import java.util.UUID;
+import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface CreditAnalysisMapper {
+    @BeforeMapping
+    default void generateId(@MappingTarget CreditAnalysisEntity creditAnalysisEntity) {
+        creditAnalysisEntity.setId(UUID.randomUUID());
+    }
+
     CreditAnalysisEntity domainToEntity(@Valid CreditAnalysisDomain creditAnalysisDomain);
 
     CreditAnalysisDomain dtoToDomain(@Valid RequestDto requestDto);
